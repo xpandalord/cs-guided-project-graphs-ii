532a984bd0be55d7bb98b59c742dab537a518b59
Andy Tran <xpandalord@gmail.com>
2020-10-15 14:33:32 -0400

    updates

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,25 +1,33 @@
 """
 An `image` is represented by a 2-D array of integers, each integer representing
 the pixel value of the image (from 0 to 65535).
-
+​
 Given a coordinate `(sr, sc)` representing the starting pixel (row and column)
 of the flood fill, and a pixel value `newColor`, "flood fill" the image.
-
+​
 To perform a "flood fill", consider the starting pixel, plus any pixels
 connected 4-directionally to the starting pixel of the same color as the
 starting pixel, plus any pixels connected 4-directionally to those pixels (also
 with the same color as the starting pixel), and so on. Replace the color of all
 of the aforementioned pixels with the newColor.
-
+​
 At the end, return the modified image.
-
+​
 Example 1:
-
+​
 ```plaintext
 Input:
-image = [[1,1,1],[1,1,0],[1,0,1]]
+image = [
+    [1,1,1],
+    [1,1,0],
+    [1,0,1]
+]
 sr = 1, sc = 1, newColor = 2
-Output: [[2,2,2],[2,2,0],[2,0,1]]
+Output: [
+    [2,2,2],
+    [2,2,0],
+    [2,0,1]
+]
 Explanation:
 From the center of the image (with position (sr, sc) = (1, 1)), all pixels
 connected by a path of the same color as the starting pixel are colored with
@@ -27,9 +35,9 @@ the new color.
 Note the bottom corner is not colored 2, because it is not 4-directionally
 connected to the starting pixel.
 ```
-
+​
 Notes:
-
+​
 - The length of `image` and `image[0]` will be in the range `[1, 50]`.
 - The given starting pixel will satisfy `0 <= sr < image.length` and
 `0 <= sc < image[0].length`.
@@ -43,9 +51,57 @@ def flood_fill(image, sr, sc, new_color):
     sr -> int
     sc -> int
     new_color -> int
-
+​
     Output:
     List[List[int]]
     """
-    # Your code here
-
+    # from the starting pixel, "radiate" out from it, coloring all pixels 
+    # that match the color of our starting pixel to `new_color`
+    # do this by checking all 4 cardinal directions from our starting pixel 
+    
+    # should we opt for DFS or BFS? 
+    # Probably doesn't matter, since we don't really care what order the 
+    # pixels get colored in 
+​
+    # what's the criteria for knowing that a pixel should not be colored in? 
+    # if the pixel's color does not match the color of the initial starting pixel 
+​
+    # 1. note the color of the starting position pixel 
+    initial_color = image[sr][sc]
+    rows = len(image)
+    cols = len(image[0])
+​
+    dft(image, sr, sc, initial_color, new_color, rows, cols)
+​
+    return image
+​
+def dft(image, sr, sc, initial_color, new_color, rows, cols):
+    # 2. set the starting pixel to `new_color` if its initial color == `initial_color`
+    if image[sr][sc] == initial_color:
+        image[sr][sc] = new_color
+    # 3. look in all 4 directions from the starting pixel
+        # look north
+        if sr >= 1:
+            dft(image, sr-1, sc, initial_color, new_color, rows, cols)
+        # look south 
+        if sr + 1 < rows:
+            dft(image, sr+1, sc, initial_color, new_color, rows, cols)
+        # look east 
+        if sc + 1 < cols:
+            dft(image, sr, sc+1, initial_color, new_color, rows, cols)
+        # look west 
+        if sc >= 1:
+            dft(image, sr, sc-1, initial_color, new_color, rows, cols)
+​
+    # 4. for each pixel whose color matches the initial color of the starting pixel 
+    # 5. set it to `new_color` 
+    # 6. recurse, passing in this pixel into our recursive function 
+​
+​
+image = [
+    [1,1,1],
+    [1,1,0],
+    [1,0,1]
+]
+​
+print(flood_fill(image, 1, 1, 2))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -1,56 +1,56 @@
 """
 In a town, there are `N` people labelled from `1` to `N`.  There is a rumor
 that one of these people is secretly the town judge.
-
+​
 If the town judge exists, then:
-
+​
 1. The town judge trusts nobody.
 2. Everybody (except for the town judge) trusts the town judge.
 3. There is exactly one person that satisfies properties 1 and 2.
-
+​
 You are given `trust`, an array of pairs `trust[i] = [a, b]` representing that
-the person labelled a trusts the person labelled `b`.
-
+the person labelled `a` trusts the person labelled `b`.
+​
 If the town judge exists and can be identified, return the label of the town
 judge.  Otherwise, return `-1`.
-
+​
 Example 1:
-
+​
 ```plaintext
 Input: N = 2, trust = [[1,2]]
 Output: 2
 ```
-
+​
 Example 2:
-
+​
 ```plaintext
 Input: N = 3, trust = [[1,3],[2,3]]
 Output: 3
 ```
-
+​
 Example 3:
-
+​
 ```plaintext
 Input: N = 3, trust = [[1,3],[2,3],[3,1]]
 Output: -1
 ```
-
+​
 Example 4:
-
+​
 ```plaintext
 Input: N = 3, trust = [[1,2],[2,3]]
 Output: -1
 ```
-
+​
 Example 5:
-
+​
 ```plaintext
 Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
 Output: 3
 ```
-
+​
 Constraints:
-
+​
 - `1 <= N <= 1000`
 - `0 <= trust.length <= 10^4`
 - `trust[i].length == 2`
@@ -63,9 +63,30 @@ def find_judge(N, trust):
     Inputs:
     N -> int
     trust -> List[List[int]]
-
+​
     Output:
     int
     """
-    # Your code here
-
+    # Your code here  
+    # In order for a town to exist, there needs to be one 
+    # node that every other node points to, and that node
+    trusts_me = [0 for _ in range(N+1)]
+    i_trust = [0 for _ in range(N+1)]
+​
+    # count the number of people who trust each person 
+    for truster, trustee in trust:
+        trusts_me[trustee] += 1
+        i_trust[truster] += 1
+​
+    # we can check to see if one person's `trust_count` == N - 1 
+    for person, trust_count in enumerate(trusts_me):
+        if trust_count == N - 1 and i_trust[person] == 0:
+            return person 
+​
+    # we also need to check that this person has no outgoing arrows 
+    # cannot have any arrows pointing out from it 
+    # and there can only be one
+    return -1 
+    
+print(find_judge(4, [[1,3],[1,4],[2,3],[2,4],[4,3]]))
+print(find_judge(3, [[1,2],[2,3]]))
